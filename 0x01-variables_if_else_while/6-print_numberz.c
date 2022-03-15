e2824db073ad08ac624a2092c7467487b5ff126f
Serge07cloud <wisdomandy.5@gmail.com>
2022-03-15 02:35:01 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Application: Numberz
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	/* New line */
+	putchar(10)
+
+	return (0);
+}
