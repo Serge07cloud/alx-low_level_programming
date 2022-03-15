db79c3d9e7241e399e8923ddc6c6c63efe529ad4
Serge07cloud <wisdomandy.5@gmail.com>
2022-03-15 01:59:17 +0000

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Application: Alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+
+	return (0);
+}
