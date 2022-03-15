1f9e674329283ed75d8161e00c8002b5ebff749d
Serge07cloud <wisdomandy.5@gmail.com>
2022-03-15 03:09:01 +0000

    Print all numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,30 +9,34 @@
 int main(void)
 {
 	int i = 0;
+	int temp = 0;
 
 	while (i < 16)
 	{
-		if (i == 10)
-		{
-			putchar(61);
-		} else if (i == 11)
-		{
-			putchar(62);
-		} else if (i == 12)
-		{
-			putchar(63);
-		} else if (i == 13)
-		{
-			putchar(64);
-		} else if (i == 14)
-		{
-			putchar(65);
-		} else if (i == 15)
-		{
-			putchar(66);
-		} else
+		if (i < 10)
 		{
 			putchar(i);
+		} else
+		{
+			if (i == 10)
+			{
+				p = 61;
+			} else if (i == 11)
+			{
+				p = 62;
+			} else if (i == 12)
+			{
+				p = 63;
+			} else if (i == 13)
+			{
+				p = 64;
+			} else if (i == 14)
+			{
+				p = 65;
+			} else
+				p = 66;
+
+			putchar(p);
 		}
 
 		i++;
