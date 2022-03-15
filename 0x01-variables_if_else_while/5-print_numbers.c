2686dd729323490014fe93d8e1b8168598e42d76
Serge07cloud <wisdomandy.5@gmail.com>
2022-03-15 02:28:25 +0000

    All digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Application: All digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 123456789;
+
+	printf("0%d\n", number);
+
+	return (0);
+}
