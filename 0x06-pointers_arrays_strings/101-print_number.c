df2300edba6f4be569d7c9631b659beaea2b3189
Serge07cloud <wisdomandy.5@gmail.com>
2022-03-26 03:15:20 +0000

    Print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_number - print an integer
+ * @n: number to print
+ *
+ * Return: nothing
+ **/
+void print_number(int n)
+{
+	int d, q, r;
+
+	d = 10;
+	q = n / d;
+	r = n % d;
+
+	if (q > 9)
+		print_number(q);
+	else
+	{
+		if (r < 0 && q < 0)
+		{
+			_putchar('-');
+			q = q * (-1);
+			r = r * (-1);
+		}
+
+		q = q + 48;
+		r = r + 48;
+
+		_putchar(q);
+		_putchar(r);
+	}
+}
