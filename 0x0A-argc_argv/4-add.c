50ccb1a2548ca19fb655407d572c654339e50d22
Serge07cloud <wisdomandy.5@gmail.com>
2022-04-04 00:07:44 +0000

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: array of pointer to string
+ *
+ * Program that add positive numbers
+ *
+ * Return: integer
+ */
+int main(int argc, char **argv)
+{
+	int sum, i;
+
+	sum = 0;
+	if (argc == 1)
+	{
+		printf("%d\n", sum);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && *argv[i] != '0')
+			{
+				printf("Error\n");
+				exit(EXIT_FAILURE);
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
+	}
+
+	exit(EXIT_SUCCESS);
+}
