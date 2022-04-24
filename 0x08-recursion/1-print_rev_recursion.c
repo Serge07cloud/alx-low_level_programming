849d79b1bd039194424dee450023b78afda7ab52
Serge07cloud <wisdomandy.5@gmail.com>
2022-04-24 09:00:57 +0000

    Print in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ /**
+  * _print_rev_recursion - function name
+  * @s: string param
+  * Description: print string in reverse
+  *
+  * Return: nothing
+  */
+void _print_rev_recursion(char *s)
+{
+	char *dst = malloc(sizeof(*s) * strlen(s));
+
+	_putchar(*(s + strlen(s) - 1));
+	if (strlen(s) > 0)
+	{
+		strncpy(dst, s, strlen(s) - 1);
+		_print_rev_recursion(dst);	
+	}
+
+	free(dst);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include <math.h>
+
+/**
+ * _sqrt_recursion - function name
+ * @n: integer param - squared number
+ * Description: give the natural square root of n
+ *
+ * Return: integer value
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt(n));
+}
