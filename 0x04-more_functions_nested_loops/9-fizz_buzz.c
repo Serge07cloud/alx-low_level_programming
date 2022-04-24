2f31ee2c36c217a0d34f3498019ceb814edd8a65
Serge07cloud <wisdomandy.5@gmail.com>
2022-04-24 00:34:58 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Fizz-Buzz programming test
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i < 101)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
