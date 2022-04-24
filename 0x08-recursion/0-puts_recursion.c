3ea4d80f201f2fad86a6c8f55ed3a6cba43a5b2e
Serge07cloud <wisdomandy.5@gmail.com>
2022-04-24 01:31:06 +0000

    Print a string followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _puts_recursion - function name
+ * @s: string param
+ * Description: prints a string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
