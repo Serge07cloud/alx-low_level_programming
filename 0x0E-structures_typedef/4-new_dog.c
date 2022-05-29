57ae93571d4a4ceb9dfb7edbb0468bc67266502c
Serge07cloud <wisdomandy.5@gmail.com>
2022-05-29 20:34:18 +0000

    Create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *store_name;
+	char *store_owner;
 
 	d = malloc(sizeof(dog_t));
+	store_name = malloc(sizeof(char) * (1 + strlen(name)));
+	store_owner = malloc(sizeof(char) * (1 + strlen(owner)));
 
-	if (d == NULL)
+	if (d == NULL || store_name == NULL || store_owner == NULL)
 	{
 		return (NULL);
 	}
-	strcpy(d->name, name);
-	strcpy(d->owner, owner);
+
+	strcpy(store_name, name);
+	strcpy(store_owner, owner);
+	d->name = store_name;
+	d->owner = store_owner;
 	d->age = age;
 
 	return (d);
