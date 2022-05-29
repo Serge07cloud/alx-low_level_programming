0e8c4dd575acea774e284de8ac08e7ca416803fa
Serge07cloud <wisdomandy.5@gmail.com>
2022-05-29 15:53:43 +0000

    Free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * free_dog - free memory allocated for a dog element
+ * @d: param of type dog_t
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+		free(d);
+}
