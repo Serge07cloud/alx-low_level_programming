1037d3a6671d7ee768f5b1470ce9d0d7f8ae6f07
Serge07cloud <wisdomandy.5@gmail.com>
2022-05-29 13:00:10 +0000

    Poppy

---
+++ 0x0E-structures_typedef/REDME.md
@@ -0,0 +1,1 @@
+Application on Structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
